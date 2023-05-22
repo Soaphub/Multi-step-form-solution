f5581a016a3974a69c47b04b313d76c9ed90449c
Soaphub <ambadimp11@gmail.com>
2023-05-22 12:48:13 +0530

    Update bootsrap link

--- pages/_document.js
+++ pages/_document.js
@@ -12,11 +12,6 @@ export default function Document() {
 			<meta name="keywords"
 			content="Frontend Mentor, Multipage form app Landing Page Challenge"/>
 			
-		  	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
-			rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
-			crossorigin="anonymous"/>
-		  	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" 
-			rel="stylesheet"/>
 		</Head>
       	<body>
     		<Main />
