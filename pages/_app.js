20ba80cf823c0b077440322aed48cda5fc778c2d
Soaphub <ambadimp11@gmail.com>
2023-05-23 12:01:07 +0530

    Made change in google font and bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,39 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import '@/styles/globals.css';
 import Layout from '@/Components/Layout';
-import Head from "next/head";
 import { store } from '../redux/store';
 import { Provider } from 'react-redux';
+import Script from 'next/script';
+import Head from "next/head";
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+
+	useEffect(()=>{
+		import("bootstrap/dist/css/bootstrap.min.css");
+	},[])
 
   	return (
 	<>	
 		<Provider store={store}>
+			
 			<Head>
-				<meta charSet="utf-8" />
-			   	<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="description" content="Website for multipage form"/>
-				<meta name="keywords" content="Frontend Mentor, Multipage form app Landing Page Challenge"/>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Website for multipage form"/>
+                <meta name="keywords" content="Frontend Mentor, Multipage form app Landing Page Challenge"/>
 
-				<title>Multi Step Form</title>
-				<link rel="icon" type="image/x-icon" href="./images/favicon-32x32.png"></link>
+                <title>Multi Step Form</title>
+                <link rel="icon" type="image/x-icon" href="./images/favicon-32x32.png"></link>
 
-			</Head>
+            </Head>
 			<Layout>
 					<Component {...pageProps} />
+					
+      	  	    	<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+			    	integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" 
+			    	crossOrigin="anonymous" />
 			</Layout>
 		
 		</Provider>
