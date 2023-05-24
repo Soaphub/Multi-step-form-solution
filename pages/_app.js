7c8311c3f2af884b0fac09c6846b0d67d209bb12
Soaphub <ambadimp11@gmail.com>
2023-05-24 15:17:08 +0530

    imporoving performance

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import Layout from '@/Components/Layout';
 import { store } from '../redux/store';
 import { Provider } from 'react-redux';
-import Script from 'next/script';
-import Head from "next/head";
 import { useEffect } from 'react';
+import Head from "next/head";
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
@@ -17,25 +16,19 @@ export default function App({ Component, pageProps }) {
   	return (
 	<>	
 		<Provider store={store}>
-			
-			<Head>
-                <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="Website for multipage form"/>
-                <meta name="keywords" content="Frontend Mentor, Multipage form app Landing Page Challenge"/>
+			<Layout>	
+					<Head>
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
+	
+						<title>Multi Step Form</title>
+						<link rel="icon" type="image/x-icon" href="./images/favicon-32x32.png"></link>
 
-                <title>Multi Step Form</title>
-                <link rel="icon" type="image/x-icon" href="./images/favicon-32x32.png"></link>
-
-            </Head>
-			<Layout>
+					</Head>
 					<Component {...pageProps} />
-					
-      	  	    	<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
-			    	integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" 
-			    	crossOrigin="anonymous" />
 			</Layout>
 		
 		</Provider>
 	</>)
 }
+
+
---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript} from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+		<Head>
+			<meta charSet="utf-8" />
+			<meta name="description" content="Website for multipage form"/>
+			<meta name="keywords" content="Frontend Mentor, Multipage form app Landing Page Challenge"/>
+		</Head>
+      	<body>
+    		<Main />
+			<NextScript />
+      	</body>
+    </Html>
+  )
+}
